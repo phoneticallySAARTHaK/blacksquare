c06df02dd91283cfdb2add0abc51050b4e97fb81
sarthak <sarthakbatham26@gmail.com>
2022-02-15 22:18:32 +0530

    Roadblock -Outdated tutorial

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # blacksquare
 A blacksquare, in wayland
 
+
 Part of the tutorial on [Writing Wayland client](https://bugaevc.gitbooks.io/writing-wayland-clients/content/)
+**NOTE:** This is outdated (using decaprated `wl\_shell`.
 
--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 #include <wayland-client.h>
+
+struct wl_compositor *compositor;
+struct wl_shm *shm;
+struct wl_shell *shell;
 
 void registry_global_handler(
 		void *data,
@@ -9,7 +14,25 @@ void registry_global_handler(
 		uint32_t version
 		)
 {
-	printf("Interface: %s, vresion: %u, name: %u\n", interface, version, name);
+	if (strcmp(interface, "wl_compositor") == 0)
+	{
+		compositor = wl_registry_bind(registry,
+				name,
+				&wl_compositor_interface,
+				3);
+	}
+	else if (strcmp(interface, "wl_shm") == 0)
+	{
+		shm = wl_registry_bind(registry, name,
+				&wl_shm_interface, 1);
+	}
+	else if (strcmp(interface, "wl_shell") == 0)
+	{
+		shell = wl_registry_bind(registry, name,
+				&wl_shell_interface, 1);
+	}
+	printf("interface: '%s', version: %u, name: %u\n",
+			interface, version, name);
 }
 
 void registry_global_remove_handler(
@@ -20,6 +43,10 @@ void registry_global_remove_handler(
 {
 	printf("Removed: %u\n", name);
 }
+const struct wl_registry_listener registry_listner = {
+		.global = registry_global_handler,
+		.global_remove = registry_global_remove_handler
+	};
 
 int main(void)
 {
@@ -32,17 +59,24 @@ int main(void)
 		return 1;
 	}
 	struct wl_registry *registry = wl_display_get_registry(display);
-	struct wl_registry_listener registry_listner = {
-		.global = registry_global_handler,
-		.global_remove = registry_global_remove_handler
-	};
 
 	wl_registry_add_listener(registry, &registry_listner, NULL);
+
+	wl_display_roundtrip(display);
+
+	if (compositor && shm && shell)
+	{
+		printf("Got 'em all\n");
+	}
+	else
+	{
+		printf("Some required globals unavailable\n");
+		return 1;
+	}
 
 	while (1)
 	{
 		wl_display_dispatch(display);
 	}
 	wl_display_disconnect(display);
-	return 0;
 }
